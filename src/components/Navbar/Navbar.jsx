d5a1d81a3a24280dda7f8913245b2bdec4ec3dc2
SarikAnwar01 <sarikanwar007@gmail.com>
2023-12-05 15:32:58 +0545

    nav-bar till date

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "./Navbar.css"; // Assuming you create a separate CSS file for styling
 import Branding from "../../assets/Branding.svg";
+import Menu from "../../assets/Union.svg";
+
 const Navbar = () => {
   return (
     <div className="navbar">
@@ -10,13 +12,27 @@ const Navbar = () => {
       </div>
       <div className="menu-container">
         <div className="upper-menu">
-          <div className="upper-menu-item">Menu 1</div>
-          <div className="upper-menu-item">Menu 2</div>
-          <div className="upper-menu-item">Menu 3</div>
+          <div className="upper-menu-item">
+            <a href="">
+              <span>Projects</span>
+            </a>
+            <a href="">
+              <span>About</span>
+            </a>
+            <a href="">
+              <span>Team</span>
+            </a>
+            <a href="">
+              <span>Contact</span>
+            </a>
+          </div>
+          <div className="upper-menu-item">
+            <img src={Menu} alt="Menu" className="menu-image" />
+          </div>
         </div>
         <div className="lower-menu">
-          <div className="lower-menu-item">Submenu 1</div>
-          <div className="lower-menu-item">Submenu 2</div>
+          <div className="lower-menu-item">Blog</div>
+          <div className="lower-menu-item">Get In Touch</div>
         </div>
       </div>
     </div>
